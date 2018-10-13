4c0928cd373cdd152a47c946de4893b30d062a64
Rahul Ramkumar <rahulram226@gmail.com>
2018-10-13 16:43:01 -0400

    Create TankDrive
    We created a tank drive program, where the left joystick corresponds with the left motor and the right joystick corresponds with the right motor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+/*
+ * Title: TankDrive
+ * Date Created: 10/13/2018
+ * Date Modified: 10/13/2018
+ * Author: Rahul
+ * Type: TeleOp
+ * Description: Left joystick moves left wheel, right joystick moves right wheel
+ */
+
+@TeleOp
+public class TankDrive extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotor leftWheel = hardwareMap.get(DcMotor.class, "leftWheel");
+        DcMotor rightWheel = hardwareMap.get(DcMotor.class, "rightWheel");
+
+        leftWheel.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        // This while loop updates the motor powers with the joystick values
+        while (opModeIsActive()) {
+            leftWheel.setPower(-gamepad1.left_stick_y);
+            rightWheel.setPower(-gamepad1.right_stick_y);
+            idle();
+        }
+    }
+}
