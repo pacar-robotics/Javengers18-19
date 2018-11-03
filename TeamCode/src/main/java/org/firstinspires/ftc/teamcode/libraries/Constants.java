ea8a8af4334808125fd7b6b0fb44174b4ca1570f
rsquared226 <rahulram226@gmail.com>
2018-11-03 10:56:29 -0400

    Add Latcher Motor to Robot Class
    The latcher motor is used for climbing on the lander
    @sarvesh30 @shivaniv26 @shamrocks721

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libraries/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libraries/Constants.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.libraries;
 /*
  * Title: Constants
  * Date Created: 10/14/2018
- * Date Modified: 10/14/2018
+ * Date Modified: 11/3/2018
  * Author: Rahul
  * Type: Library
  * Description: This will contain all of the constants we will use in any of our programs.
@@ -14,4 +14,5 @@ public class Constants {
     //********** DcMotor Indexes **********//
     final static int LEFT_WHEEL = 0;
     final static int RIGHT_WHEEL = 1;
+    final static int LATCHER = 2;
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libraries/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libraries/Robot.java
@@ -4,13 +4,14 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+import static org.firstinspires.ftc.teamcode.libraries.Constants.LATCHER;
 import static org.firstinspires.ftc.teamcode.libraries.Constants.LEFT_WHEEL;
 import static org.firstinspires.ftc.teamcode.libraries.Constants.RIGHT_WHEEL;
 
 /*
  * Title: Robot
  * Date Created: 10/14/2018
- * Date Modified: 10/14/2018
+ * Date Modified: 11/3/2018
  * Author: Rahul
  * Type: Library
  * Description: This is the base library for any main op to be based off. It will contain all the
@@ -21,7 +22,7 @@ public class Robot {
 
     private LinearOpMode opMode;
 
-    private DcMotor[] dcMotors = new DcMotor[2];
+    private DcMotor[] dcMotors = new DcMotor[3];
 
     Robot(LinearOpMode opMode) {
         this.opMode = opMode;
@@ -32,6 +33,7 @@ public class Robot {
     private void initDcMotor() {
         dcMotors[LEFT_WHEEL] = opMode.hardwareMap.get(DcMotor.class, "leftWheel");
         dcMotors[RIGHT_WHEEL] = opMode.hardwareMap.get(DcMotor.class, "rightWheel");
+        dcMotors[LATCHER] = opMode.hardwareMap.get(DcMotor.class, "latcher");
 
         dcMotors[LEFT_WHEEL].setDirection(DcMotorSimple.Direction.REVERSE);
     }
