fd14cd43d013b1e134357c94b38910ac538362a6
rsquared226 <rahulram226@gmail.com>
2018-11-03 11:39:46 -0400

    Change Order of Constant Modifiers

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libraries/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libraries/Constants.java
@@ -12,11 +12,11 @@ package org.firstinspires.ftc.teamcode.libraries;
 public class Constants {
 
     //********** Gamepad Tolerance Constants **********//
-    final static float GAMEPAD_JOYSTICK_TOLERANCE = .05f;
-    final static float GAMEPAD_TRIGGER_TOLERANCE = .05f;
+    static final float GAMEPAD_JOYSTICK_TOLERANCE = .05f;
+    static final float GAMEPAD_TRIGGER_TOLERANCE = .05f;
 
     //********** DcMotor Indexes **********//
-    final static int LEFT_WHEEL = 0;
-    final static int RIGHT_WHEEL = 1;
-    final static int LATCHER = 2;
+    static final int LEFT_WHEEL = 0;
+    static final int RIGHT_WHEEL = 1;
+    static final int LATCHER = 2;
 }
