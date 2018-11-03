9c379e87e5c6799ef7bd329fbe7988b014191e40
rsquared226 <rahulram226@gmail.com>
2018-11-03 17:15:32 -0400

    Add Autonomous Annotation
    It needs that to run
    
    Co-Authored-By: 24pparikh <24pparikh@users.noreply.github.com>
    Co-Authored-By: varni413 <varni413@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainops/MainAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainops/MainAutonomous.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.mainops;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.libraries.AutoLib;
@@ -13,6 +14,7 @@ import org.firstinspires.ftc.teamcode.libraries.AutoLib;
  * Description: The main autonomous program
  */
 
+@Autonomous(group = "Main")
 public class MainAutonomous extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
