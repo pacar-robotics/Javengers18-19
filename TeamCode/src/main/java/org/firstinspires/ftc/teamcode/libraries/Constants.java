148428f54503d06b1c05ea573b3fedad8647bd43
varni413 <varnikaudhay@gmail.com>
2018-11-10 11:45:46 -0500

    Change track distnace
    Co-Authored-By: 24pparikh <24pparikh@users.noreply.github.com>
    Co-Authored-By: Rahul Ramkumar <rsquared226@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libraries/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libraries/Constants.java
@@ -4,7 +4,7 @@ package org.firstinspires.ftc.teamcode.libraries;
  * Title: Constants
  * Date Created: 10/14/2018
  * Date Modified: 11/3/2018
- * Author: Rahul
+ * Author: Rahul, Varnika, Poorvi
  * Type: Library
  * Description: This will contain all of the constants we will use in any of our programs.
  */
@@ -24,5 +24,5 @@ public class Constants {
     static final float WHEEL_DIAMETER = 10.16f; // Centimeters
     static final float GOBILDA_MOTOR_ENCODER_COUNTS_PER_REVOLUTION = 1425.2f;
     static final float ENCODER_MARGIN = 50f;
-    static final float TRACK_DISTANCE = 16f;
+    static final float TRACK_DISTANCE = 20f;
 }
