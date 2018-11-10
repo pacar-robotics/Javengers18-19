f7ff9d4c7a37eec8853526c3c417e11b6cc0aae3
varni413 <varnikaudhay@gmail.com>
2018-11-10 11:53:16 -0500

    Changed track distance
    Co-Authored-By: 24pparikh <24pparikh@users.noreply.github.com>
    Co-Authored-By: Rahul Ramkumar <rsquared226@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libraries/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libraries/Constants.java
@@ -24,5 +24,5 @@ public class Constants {
     static final float WHEEL_DIAMETER = 10.16f; // Centimeters
     static final float GOBILDA_MOTOR_ENCODER_COUNTS_PER_REVOLUTION = 1425.2f;
     static final float ENCODER_MARGIN = 50f;
-    static final float TRACK_DISTANCE = 20f;
+    static final float TRACK_DISTANCE = 36.5f;
 }
