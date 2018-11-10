84d10be6cdd288fdb6ba051a37fa04afd63297b2
rsquared226 <rahulram226@gmail.com>
2018-11-10 11:37:03 -0500

    Start CalcTurn
    It is not accurate at this moment, but it will be fixed
    
    Co-Authored-By: 24pparikh <24pparikh@users.noreply.github.com>
    Co-Authored-By: varni413 <varni413@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libraries/AutoLib.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libraries/AutoLib.java
@@ -9,6 +9,7 @@ import static org.firstinspires.ftc.teamcode.libraries.Constants.GOBILDA_MOTOR_E
 import static org.firstinspires.ftc.teamcode.libraries.Constants.LATCHER;
 import static org.firstinspires.ftc.teamcode.libraries.Constants.LEFT_WHEEL;
 import static org.firstinspires.ftc.teamcode.libraries.Constants.RIGHT_WHEEL;
+import static org.firstinspires.ftc.teamcode.libraries.Constants.TRACK_DISTANCE;
 import static org.firstinspires.ftc.teamcode.libraries.Constants.WHEEL_DIAMETER;
 
 /*
@@ -34,7 +35,7 @@ public class AutoLib {
         final int targetPosition = (int) ((centimeters / (Math.PI * WHEEL_DIAMETER)) *
                 GOBILDA_MOTOR_ENCODER_COUNTS_PER_REVOLUTION);
 
-        prepMotorsForCalcMove(targetPosition);
+        prepMotorsForCalcMove(targetPosition, targetPosition);
 
         robot.setDcMotorPower(LEFT_WHEEL, power);
         robot.setDcMotorPower(RIGHT_WHEEL, power);
@@ -49,15 +50,48 @@ public class AutoLib {
         robot.setDcMotorPower(RIGHT_WHEEL, 0);
     }
 
-    private void prepMotorsForCalcMove(int targetPosition) {
+    public void calcTurn(int degrees, float power) {
+        int leftTargetPosition = (int) (2 * ((TRACK_DISTANCE) * degrees
+                * GOBILDA_MOTOR_ENCODER_COUNTS_PER_REVOLUTION) /
+                (WHEEL_DIAMETER * 360));
+        int rightTargetPosition = -leftTargetPosition;
+
+        float leftPower;
+        float rightPower;
+
+        // Will switch powers if turning the other way
+        if (degrees < 0) {
+            leftPower = power;
+            rightPower = -power;
+        } else {
+            leftPower = -power;
+            rightPower = power;
+        }
+
+        prepMotorsForCalcMove(leftTargetPosition, rightTargetPosition);
+
+        robot.setDcMotorPower(LEFT_WHEEL, leftPower);
+        robot.setDcMotorPower(RIGHT_WHEEL, rightPower);
+
+        // Stays in this while loop until the motors are done moving to their position
+        while (areBaseMotorsBusy() &&
+                (Math.abs(leftTargetPosition - robot.getDcMotorPosition(LEFT_WHEEL)) >= ENCODER_MARGIN)) {
+            opMode.idle();
+        }
+
+        robot.setDcMotorPower(LEFT_WHEEL, 0);
+        robot.setDcMotorPower(RIGHT_WHEEL, 0);
+    }
+
+    private void prepMotorsForCalcMove(int leftTargetPosition, int rightTargetPosition) {
         robot.setDcMotorMode(LEFT_WHEEL, STOP_AND_RESET_ENCODER);
         robot.setDcMotorMode(RIGHT_WHEEL, STOP_AND_RESET_ENCODER);
 
         robot.setDcMotorMode(LEFT_WHEEL, RUN_TO_POSITION);
         robot.setDcMotorMode(RIGHT_WHEEL, RUN_TO_POSITION);
 
-        robot.setDcMotorTargetPosition(LEFT_WHEEL, targetPosition);
-        robot.setDcMotorTargetPosition(RIGHT_WHEEL, targetPosition);
+        robot.setDcMotorTargetPosition(LEFT_WHEEL, leftTargetPosition);
+        robot.setDcMotorTargetPosition(RIGHT_WHEEL, rightTargetPosition);
     }
 
     private boolean areBaseMotorsBusy() {
