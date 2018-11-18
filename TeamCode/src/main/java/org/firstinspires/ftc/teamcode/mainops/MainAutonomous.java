35527f4ac70add7a935d8275a6f0709167b3c907
rsquared226 <rahulram226@gmail.com>
2018-11-18 17:30:41 -0500

    Fix Formatting and Update Header

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainops/MainAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainops/MainAutonomous.java
@@ -8,7 +8,7 @@ import org.firstinspires.ftc.teamcode.libraries.AutoLib;
 /*
  * Title: MainAutonomous
  * Date Created: 11/3/2018
- * Date Modified: 11/4/2018
+ * Date Modified: 11/18/2018
  * Author: Rahul, Poorvi, Varnika
  * Type: Main
  * Description: The main autonomous program
@@ -34,12 +34,12 @@ public class MainAutonomous extends LinearOpMode {
         //Blue depot base parking near the robot picture
 
         autoLib.landOnGround();
-       autoLib.calcMove(33, .2f );
+        autoLib.calcMove(33, .2f);
         Thread.sleep(100);
-        autoLib.calcTurn(80,.2f);
+        autoLib.calcTurn(80, .2f);
         Thread.sleep(100);
-        autoLib.calcMove(87,.5f );
-        autoLib.calcTurn(40, .5f );
-       autoLib.calcMove(68,.5f);
+        autoLib.calcMove(87, .5f);
+        autoLib.calcTurn(40, .5f);
+        autoLib.calcMove(68, .5f);
     }
 }
