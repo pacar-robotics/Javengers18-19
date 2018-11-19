6fde75a3831fe7164df490ef6ee465031a1e53e9
rsquared226 <rahulram226@gmail.com>
2018-11-18 20:48:29 -0500

    Make Program to Automatically Move Latcher to Max Position
    This is useful for autonomous, when the latcher motor needs to hang off the lander

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/supportops/MoveLatcherToTop.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.supportops;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.libraries.AutoLib;
+
+/*
+ * Title: MoveLatcherToTop
+ * Date Created: 11/18/2018
+ * Date Modified: 11/18/2018
+ * Author: Rahul
+ * Type: Support
+ * Description: This will move the latcher motor to the top until the touch sensor is pressed
+ */
+
+@Autonomous(group = "Support")
+public class MoveLatcherToTop extends LinearOpMode {
+    private AutoLib autoLib;
+
+    @SuppressWarnings("RedundantThrows")
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initialize();
+        autoLib.moveLatcherToTop();
+    }
+
+    private void initialize() {
+        telemetry.addData("Status", "Initializing...");
+        telemetry.update();
+
+        autoLib = new AutoLib(this);
+
+        telemetry.addData("Status", "Ready");
+        telemetry.update();
+        waitForStart();
+
+        telemetry.addData("Status", "Running");
+        telemetry.update();
+    }
+}
