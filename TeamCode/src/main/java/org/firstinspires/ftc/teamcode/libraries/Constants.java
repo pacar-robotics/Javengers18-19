02fd80611140c4b31c1d311adb8c2a123e36323a
rsquared226 <rahulram226@gmail.com>
2018-12-23 17:01:32 -0500

    Update Vuforia Key
    Previous key did not support external webcams
    
    Co-Authored-By: 24pparikh <24pparikh@users.noreply.github.com>
    Co-Authored-By: varni413 <varni413@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libraries/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libraries/Constants.java
@@ -59,7 +59,7 @@ public class Constants {
     static final float LATCHING_DRIVE_FACTOR = 0.5f;
 
     //********** TensorFlow **********//
-    static final String VUFORIA_KEY = "AevlBL3/////AAAAGZ3T16bk1EepnUsSLPkQW/sFqYxxQLGZ0w6paGMug92slctEFAuXjXeMqrzDuCLvLZmY1sWjvn4kb5WKPKH4RdCZB7ccft3XGKh8rVn0r+TxhcJUmZwsdciAzCBYVe5FLnGtldKTV1eVbNFcN6FpDfZstRXXBdjqyMBg5XzJmhJp5rcG5TIi0qMcjaoHFqaBdnMyYBAeERylDVGBbDbIAX0dLDiQ5bjxA/lAphyHjDDyetpVjGlEwziUzcYbdvZK3zjGpR7WH62RqM6QzO1s7PcTppQMgRi3FxhisqKKZdWWF5pFGBPMP6bpsOzHTd8TDxPjwXiYIZxt3MwkhQ+1JpyAG9CVo+I0T/b/oNT0/ulZ";
+    static final String VUFORIA_KEY = "AXKAgZb/////AAABmUS4ua80h0IOpsYtCk/GLTBlnQxMgwAqHFWy/jk/+c1LzWYsECeqyVFo8lr8efZWyITbeMxf4IhhNmROQ3O13t8pqcHIOhDPfp82HmA54VHiYCt3n3xpcz/xi0Vs1mh9/xe0L1coIN1cL8EnkLHVkziy8jhQV5piuKMYeqxhY0tg0OekmPBb41Bw+LE8a76/dNJjAW/uCjefuWo0pBYETldIdqnyjeRP6hmfJbo9LbSOT70zmH4fFbMKJiYnz543nF9/SpxjkNnrgjijP7f9FV98Mdhgr7f6uyqMV37oFIQIhkCh5ro737KfXQyvWfPDedbEDFy3Z2duM9JzYJA8ybFKozEtjUeB82XQgaPeDNwS";
     static final float TENSOR_READING_TIME = 2f;   // Seconds
 
     public enum GoldObjectPosition {LEFT, CENTER, RIGHT}
