fe4c50ba15023410931dae18baabf69f0e1d9341
varni413 <varnikaudhay@gmail.com>
2019-01-01 18:28:47 -0500

    Updating constants
    Co-Authored-By: 24pparikh <24pparikh@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libraries/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/libraries/Constants.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.libraries;
 /*
  * Title: Constants
  * Date Created: 10/14/2018
- * Date Modified: 11/27/2018
+ * Date Modified: 1/1/2019
  * Author: Rahul, Poorvi, Varnika, Sarvesh, Sachin, Shivani
  * Type: Library
  * Description: This will contain all of the constants we will use in any of our programs.
@@ -19,15 +19,15 @@ public class Constants {
     static final int MOTOR_LEFT_WHEEL = 0;
     static final int MOTOR_RIGHT_WHEEL = 1;
     static final int MOTOR_LATCHER = 2;
-    static final int MOTOR_LINEAR_SLIDE = 4;
-    static final int MOTOR_SCORING = 5;
+    static final int MOTOR_LINEAR_SLIDE = 3;
+    static final int MOTOR_SCORING = 0;
 
     //********** Servo Indexes **********//
     static final int SERVO_LATCHER = 0;
     static final int SERVO_INTAKE = 1;
-    static final int SERVO_INTAKE_ANGLE = 2;
-    static final int SERVO_DEPOSIT = 3;
-    static final int SERVO_DEPOSIT_ANGLE = 4;
+    static final int SERVO_INTAKE_ANGLE = 0;
+    static final int SERVO_DEPOSIT = 2;
+    static final int SERVO_DEPOSIT_ANGLE = 3;
 
     //********** Touch Sensor Indexes **********//
     static final int TOUCH_LATCHER_TOP = 0;
@@ -61,8 +61,9 @@ public class Constants {
     static final float LATCHING_DRIVE_FACTOR = 0.5f;
 
     //********** TensorFlow **********//
-    static final String VUFORIA_KEY = "AevlBL3/////AAAAGZ3T16bk1EepnUsSLPkQW/sFqYxxQLGZ0w6paGMug92slctEFAuXjXeMqrzDuCLvLZmY1sWjvn4kb5WKPKH4RdCZB7ccft3XGKh8rVn0r+TxhcJUmZwsdciAzCBYVe5FLnGtldKTV1eVbNFcN6FpDfZstRXXBdjqyMBg5XzJmhJp5rcG5TIi0qMcjaoHFqaBdnMyYBAeERylDVGBbDbIAX0dLDiQ5bjxA/lAphyHjDDyetpVjGlEwziUzcYbdvZK3zjGpR7WH62RqM6QzO1s7PcTppQMgRi3FxhisqKKZdWWF5pFGBPMP6bpsOzHTd8TDxPjwXiYIZxt3MwkhQ+1JpyAG9CVo+I0T/b/oNT0/ulZ";
+    static final String VUFORIA_KEY = "AXKAgZb/////AAABmUS4ua80h0IOpsYtCk/GLTBlnQxMgwAqHFWy/jk/+c1LzWYsECeqyVFo8lr8efZWyITbeMxf4IhhNmROQ3O13t8pqcHIOhDPfp82HmA54VHiYCt3n3xpcz/xi0Vs1mh9/xe0L1coIN1cL8EnkLHVkziy8jhQV5piuKMYeqxhY0tg0OekmPBb41Bw+LE8a76/dNJjAW/uCjefuWo0pBYETldIdqnyjeRP6hmfJbo9LbSOT70zmH4fFbMKJiYnz543nF9/SpxjkNnrgjijP7f9FV98Mdhgr7f6uyqMV37oFIQIhkCh5ro737KfXQyvWfPDedbEDFy3Z2duM9JzYJA8ybFKozEtjUeB82XQgaPeDNwS";
     static final float TENSOR_READING_TIME = 2f;   // Seconds
 
     public enum GoldObjectPosition {LEFT, CENTER, RIGHT}
 }
+
